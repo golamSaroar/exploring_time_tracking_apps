30a1272bb55ff42b2ce2496041692ecb6c6795de
Sk Golam Saroar <golamSaroar@users.noreply.github.com>
2024-03-30 14:01:03 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,32 +8,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-import log
+from log import write, print_progress_bar
 
 URL = "https://marketplace.atlassian.com/search?category=Time%20tracking&product=jira"
-
-
-# Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
 
 
 def get_field_text_if_exists(item, selector):
@@ -55,8 +32,6 @@ def get_link_if_exists(item, selector):
 chrome_options = Options()
 chrome_options.add_argument('--blink-settings=imagesEnabled=false')
 chrome_options.add_argument("--incognito")
-# chrome_options.add_argument("--window-size=1920x1080")
-
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='./chromedriver')
 wait = WebDriverWait(driver, 5)
 driver.get(URL)
@@ -84,27 +59,31 @@ with open("data.csv", "w") as f:
     w = csv.DictWriter(f, header)
     w.writeheader()
 
+
 print("Reading data from the list page.")
-printProgressBar(0, len(driver.find_elements_by_css_selector('.css-187scic-LargeHitContainer')), prefix = 'Progress:', suffix = 'Complete', length = 50)
+items = driver.find_elements_by_css_selector('.css-187scic-LargeHitContainer')
+print_progress_bar(0, len(items), prefix='Progress:', suffix='Complete', length=50)
 # get data from the list page (given URL) and add to the dictionary
-for i, result in enumerate(driver.find_elements_by_css_selector('.css-187scic-LargeHitContainer')):
+for i, result in enumerate(items):
     name = get_field_text_if_exists(result, '.css-r7ms9i-H3-baseHeadingsCss-baseHeadingsCss-h3Css-h3Css-NameHeader')
     description = get_field_text_if_exists(result, '.css-1lqn5mv-Body')
     categories = get_field_text_if_exists(result, '.css-1hj6rt6-CategoriesStyled')
     link = get_link_if_exists(result, '.css-tgmll2-linkCss-linkCss-HitLinkStyled-HitLinkStyled')
+
     app_id = re.search("([^/]+)(?=/[^/]+/?$)", link).group(0)
     results[app_id] = {"name": name, "description": description, "categories": categories, "link": link}
-    printProgressBar(i+1, len(driver.find_elements_by_css_selector('.css-187scic-LargeHitContainer')), prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    print_progress_bar(i + 1, len(items), prefix='Progress:', suffix='Complete', length=50)
+
 print("Completed reading data from the list page! Starting to crawl item pages.")
 
-# Initial call to print 0% progress
-printProgressBar(0, len(results), prefix = 'Progress:', suffix = 'Complete', length = 50)
+print_progress_bar(0, len(results), prefix='Progress:', suffix='Complete', length=50)
 # go to each of the plugin pages and gather data from there
-for i, key in enumerate(results, start = 1):
+for i, key in enumerate(results, start=1):
     driver.get(results[key]["link"])
-    
-    log.write("Row {} : {}".format(i, results[key]["link"]))
-    
+
+    write("Row {} : {}".format(i, results[key]["link"]))
+
     results[key]["vendor_name"] = get_field_text_if_exists(driver, '.plugin-vendor-name')
     results[key]["total_installs"] = get_field_text_if_exists(driver, ".plugin-active-installs-total")
     results[key]["total_reviews"] = get_field_text_if_exists(driver, ".plugin-rating-summary .badge")
@@ -115,23 +94,26 @@ for i, key in enumerate(results, start = 1):
     results[key]["is_jira_service"] = get_field_text_if_exists(driver, ".plugin-jira-comp")
 
     try:
-        # pricing = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#cloud-pricing .pup-pricing-block-amount")))
-        for idx, r in enumerate(driver.find_elements_by_css_selector("#cloud-pricing .pup-pricing-block-amount"), start = 1):
+        cloud_pricing = wait.until(EC.presence_of_all_elements_located((
+            By.CSS_SELECTOR, "#cloud-pricing .pup-pricing-block-amount")))
+        for idx, r in enumerate(driver.find_elements_by_css_selector("#cloud-pricing .pup-pricing-block-amount"),
+                                start=1):
             results[key]["pricing_cloud_{}".format(idx)] = r.get_attribute("innerText")
     except TimeoutException:
         pass
-    
+
     try:
-        server_pricing = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#server-pricing .amount strong")))
-        for idx, r in enumerate(driver.find_elements_by_css_selector("#server-pricing .amount strong"), start = 1):
+        server_pricing = wait.until(EC.presence_of_all_elements_located((
+            By.CSS_SELECTOR, "#server-pricing .amount strong")))
+        for idx, r in enumerate(driver.find_elements_by_css_selector("#server-pricing .amount strong"), start=1):
             results[key]["pricing_server_{}".format(idx)] = r.get_attribute("innerText")
     except TimeoutException:
         pass
 
-    with open("data.csv", "a") as f:    
+    with open("data.csv", "a") as f:
         w = csv.DictWriter(f, header)
         w.writerow(results[key])
 
-    printProgressBar(i + 1, len(results), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    print_progress_bar(i + 1, len(results), prefix='Progress:', suffix='Complete', length=50)
 
 driver.quit()
